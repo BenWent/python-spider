74ffd985ad3749993a6aede8ded1048a1049eb9b
BenWent <1914351357@qq.com>
2019-02-16 12:03:26 +0800

    修正程序book_analysis.py，抓取数据存储在remark_info.json中

--- book_analysis.py
+++ book_analysis.py
@@ -4,7 +4,8 @@ from lxml import etree
 from datetime import datetime
 import json
 
-import matplotlib.pyplot as plt
+from functools import reduce
+import pylab as plt
 
 ## 分析红楼梦的评分信息
 
@@ -14,8 +15,8 @@ class RemarkInfo:
 		# 出现的次数
 		self.rank = rank
 		self.date = date
-
 		self.count = count
+
 
 	def __eq__(self, other):
 		self_year = int(
@@ -43,7 +44,7 @@ def remarkInfoToDict(remark_info):
 
 # 将 dict对象 转换为 RemarkInfo对象
 def dictToRemarkInfo(remarkInfoDict):
-	return Person(remarkInfoDict['rank'], remarkInfoDict['date'], \
+	return RemarkInfo(remarkInfoDict['rank'], remarkInfoDict['date'], \
 		remarkInfoDict['count'])
 
 if __name__ == '__main__':
@@ -74,7 +75,7 @@ if __name__ == '__main__':
 			# //*[@id="comments"]/ul/li[13]/div[2]/h3/span[2]/span[1]
 			# //*[@id="comments"]/ul/li[13]/div[2]/h3/span[2]/span[2]
 			# //*[@id="comments"]/ul/li[12]/div[2]/h3/span[2]/span
-			//*[@id="comments"]/div[9]/div[2]/h3/span[2]/span[3]
+			# //*[@id="comments"]/div[9]/div[2]/h3/span[2]/span[3]
 			info_resp = requests.get(base_url % i)
 			info_html = etree.HTML(info_resp.text)
 			# 请求数据
@@ -88,7 +89,7 @@ if __name__ == '__main__':
 				info_span_list = info_li.xpath('div[2]/h3/span[2]/span')
 				if len(info_span_list) == 2:
 					info_rank = int(info_span_list[0].xpath('@class')[0] \
-						.split(' ')[0] \
+						.split(' ')[1] \
 						.replace('allstar', '')) // 10
 					info_date = info_span_list[1].xpath('text()')[0].strip()
 
@@ -101,30 +102,86 @@ if __name__ == '__main__':
 
 		# 将爬取的数据序列化到 remark_info.json文件 中
 		with open('remark_info.json', 'w') as fp:
-			json.dump(remark_info_list, fp, default=remarkInfoDict)
+			json.dump(remark_info_list, fp, default=remarkInfoToDict)
 
 
 	# 绘图分析
 
 	## 1、按分数统计
-	ranks = (i for i in range(1, 6)) # 1star 至于 5star
+	ranks = [i for i in range(1, 6)] # 1star 至于 5star
 	counts = [0 for i in range(len(ranks) + 1)]
+	## 2、按年份统计
+	star_dict_group_by_year = {}
 
 	# 统计
 	for remark_info in remark_info_list:
-		counts[remark_info.rank] += 1
+		counts[remark_info.rank] += remark_info.count
+
+		if remark_info.rank == 5: # 只统计5-star
+			star_dict_group_by_year[int(
+				datetime.strptime(remark_info.date, '%Y-%m-%d') \
+					.timetuple().tm_year
+			)] = remark_info.count
+
 	# 删除 counts的第一个元素
 	del counts[0]
 
 	# 直方图
+	plt.figure(1)
+
+	plt.subplot(121)
 	plt.bar(ranks, counts, align='center', color='purple', alpha=0.8)
 	plt.xlabel('star-number')
 	plt.ylabel('people-number')
 	plt.title('A Dream in Red Mansions')
-
 	for x, y in enumerate(counts):
-		plt.text(x, y+200, '%s' %round(y, 1), ha='center', va='baseline')
+		plt.text(x+1, y+20, '%s' %(y), ha='center', va='baseline')
+
+	# 按年份统计 5-star，绘制饼状图
+	# 处理数据
+	years = []
+	nums = []
+	for key, value in sorted(star_dict_group_by_year.items(), reverse=True):
+		years.append(key)
+		nums.append(value)
+
+	percentage_nums = list(map(lambda x, y: round(x/y, 4), nums, 
+		(reduce(lambda x, y: x+y, nums) 
+			for i in range(len(nums)))
+		))
+
+	explode = [0 for i in range(len(years))]
+	explode[0] = 0.2 # 突出显示最新年份的数据
+	explode[nums.index(max(nums))] = 0.2 # 突出显示评价最高的年份
+
+	# for x, y, z in zip(years, nums, percentage_nums):
+	# 	print(x, y, z)
+
+	# 绘制按年份5-star统计的直方图
+	plt.subplot(122)
+	plt.bar(years, nums, align='center', color='purple', alpha=0.8)
+	plt.xlabel('year')
+	plt.ylabel('5star-number')
+	plt.title('A Dream in Red Mansions')
+	for x, y in zip(years, nums):
+		plt.text(x, y+20, '%s' %(y), ha='center', va='baseline')
+
+	# 绘制按年份5-star统计的饼状图
+	plt.figure(2)
+	plt.style.use('ggplot')
+	plt.xlim(0, 1)
+	plt.ylim(0, 1)
+	plt.axes(aspect='equal')
+	plt.pie(x=percentage_nums, # 百分比数据
+		labels=years, # 文字标签
+		explode=explode, # 突出显示某部分数据
+		pctdistance=0.8, # 设置百分比标签与圆心的距离
+		labeldistance=1.15, # 设置文字标签与圆心的距离
+		autopct='%.2f%%', # 设置百分比保留两位小数
+	) 
 
 	plt.show()
 
 
+# 从最终的图标中可以看出
+## 1、对《红楼梦》的关注主要开始与2007，到2012年达到最高峰
